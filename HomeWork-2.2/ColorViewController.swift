800f2d5a25bb04c2b82e8226d21132ec7bf03a03
Falikor <Bugorv90@mail.ru>
2021-06-06 00:10:47 +0300

    aded update color value

--- HomeWork-2.2/ColorViewController.swift
+++ HomeWork-2.2/ColorViewController.swift
@@ -7,7 +7,28 @@
 
 import UIKit
 
-class ColorViewController: UIViewController {
+protocol UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField, sender: UISlider)
+}
+
+class ColorViewController: UIViewController, UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField, sender: UISlider) {
+        textField.text = String(format: "%.2f", sender.value)
+        switch textField {
+        case refTextField:
+            redLabel.text = String(format: "%.2f", sender.value)
+        case greenTextField:
+            greenLabel.text = String(format: "%.2f", sender.value)
+        case blueTextField:
+            blueLabel.text = String(format: "%.2f", sender.value)
+        case alphaTextFild:
+            alfaLabel.text = String(format: "%.2f", sender.value)
+        default:
+            print("Error")
+        }
+    }
+    
     
 // added utlet View, Labels, Sliders
     
@@ -25,12 +46,40 @@ class ColorViewController: UIViewController {
     @IBOutlet weak var bSlaider: UISlider!
     @IBOutlet weak var aSlaider: UISlider!
     
+// All TextFild
+    @IBOutlet weak var refTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    @IBOutlet weak var alphaTextFild: UITextField!
+    
+    
     var mainColor: UIColor!
     var delegate: SettingsViewControllerDelegate!
     
     
     override func viewDidLoad() {
         viewPrint.backgroundColor = mainColor
+        getColorValue(UIColor: mainColor)
+        
+    }
+    private func getColorValue(UIColor: UIColor) {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        UIColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        rSlider.value = Float(red)
+        gSlaider.value = Float(green)
+        bSlaider.value = Float(blue)
+        aSlaider.value = Float(alpha)
+        
+        redLabel.text = String(format: "%.2f", rSlider.value)
+        greenLabel.text = String(format: "%.2f", gSlaider.value)
+        blueLabel.text = String(format: "%.2f", bSlaider.value)
+        alfaLabel.text = String(format: "%.2f", aSlaider.value)
+        
+        refTextField.text = String(format: "%.2f", rSlider.value)
+        greenTextField.text = String(format: "%.2f", gSlaider.value)
+        blueTextField.text = String(format: "%.2f", bSlaider.value)
+        alphaTextFild.text = String(format: "%.2f", aSlaider.value)
+        
     }
 // creat func change color and alpha
     private func changColor() {
@@ -38,24 +87,23 @@ class ColorViewController: UIViewController {
         viewPrint.backgroundColor = mainColor
     }
  
-    
 // creat action sliders wich use func change color and sed valeus in label
     @IBAction func redSender(_ sender: UISlider) {
         changColor()
-        redLabel.text = String((sender.value * 100).rounded() / 100)
+        textFieldDidEndEditing(refTextField, sender: sender)
     }
     @IBAction func greenSender(_ sender: UISlider) {
         changColor()
-        greenLabel.text = String((sender.value * 100).rounded() / 100)
+        textFieldDidEndEditing(greenTextField, sender: sender)
 
     }
     @IBAction func blueSender(_ sender: UISlider) {
         changColor()
-        blueLabel.text = String((sender.value * 100).rounded() / 100)
+        textFieldDidEndEditing(blueTextField, sender: sender)
     }
     @IBAction func alfaSender(_ sender: UISlider) {
         changColor()
-        alfaLabel.text = String((sender.value * 100).rounded() / 100)
+        textFieldDidEndEditing(alphaTextFild, sender: sender)
     }
     
     @IBAction func doneButton(_ sender: UIButton) {
